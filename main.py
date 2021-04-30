936866f5f6632ac6f7f13bfc9e64c519d3618560
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-04-30 19:52:06 +0530

    Reply Markup only for bots

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ from pyrogram.errors import FloodWait
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 CHAT_IDS = list(set(int(x) for x in os.environ.get("CHAT_IDS", None).split()))
 EDIT_FORMAT = os.environ.get("EDIT_FORMAT")
-REPLY_MARKUP = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Join Updates Channel', url="https://telegram.me/FayasNoushad")
-        ]]
-    )
 
 FayasNoushad = Client(
     "Channel Message Editor",
@@ -34,41 +29,14 @@ async def edit_caption(client, message):
             await message.forward(LOG_CHANNEL)
             return
         if int(message.chat.id) in CHAT_IDS:
-            if REPLY_MARKUP:
-                await client.edit_message_caption(
-                    chat_id=message.chat.id,
-                    message_id=message.message_id,
-                    caption=EDIT_FORMAT.format(message.text),
-                    reply_markup=REPLY_MARKUP
-                )
-            else:
-                await client.edit_message_caption(
-                    chat_id=message.chat.id,
-                    message_id=message.message_id,
-                    caption=EDIT_FORMAT.format(message.text)
-                )
+            await client.edit_message_caption(
+                chat_id=message.chat.id,
+                message_id=message.message_id,
+                caption=EDIT_FORMAT.format(message.text)
+            )
     except FloodWait as floodwait:
         await asyncio.sleep(floodwait.x)
         return edit_caption(client, message)
-    except Exception as error:
-        print(error)
-
-@FayasNoushad.on_message(filters.text & filters.channel)
-async def edit_text(client, message):
-    try:
-        if LOG_CHANNEL:
-            await message.forward(LOG_CHANNEL)
-            return
-        if int(message.chat.id) in CHAT_IDS:
-            if REPLY_MARKUP:
-                await client.edit_message_reply_markup(
-                    chat_id=message.chat.id,
-                    message_id=message.message_id,
-                    reply_markup=REPLY_MARKUP
-                )
-    except FloodWait as floodwait:
-        await asyncio.sleep(floodwait.x)
-        return edit_text(client, message)
     except Exception as error:
         print(error)
 
